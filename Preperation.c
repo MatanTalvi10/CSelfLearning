bebe0cd46a3e4d4509dba683015503588f2edc2b
MatanTalvi10 <matantalbi@gmail.com>
2022-07-13 17:15:00 +0300

    update

--- Preperation.c
+++ Preperation.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
-
+#include<string.h>
+void print(char c[]){
+    c[2] = 'a';
+    while (*c != '\0')
+    {
+        printf("%c\n", *c);
+        c++;
+    }
+    c[2] = 't';
+    
+}
 int main()
 {
-    printf("%d", 10);
+    char c[20] = "hello";
+    print(c);
+    printf("%s\n", c);
     return 0;
 }
