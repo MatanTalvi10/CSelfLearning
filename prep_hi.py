97f2456eafd2e218dada101df207b050206b4cff
MatanTalvi10 <matantalbi@gmail.com>
2022-07-15 21:09:20 +0300

    links

--- prep_hi.py
+++ prep_hi.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import plotly.express as px
 import numpy as np
 import pandas as pd
-a = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
-print("hello")
-print(a)
+np.random.seed(162)
+N = 25
+x = np.random.rand(N)*100
+print(x)
